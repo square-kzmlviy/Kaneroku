8683f0039ced24663f92e85e45fd1e96fc7a2254
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-22 17:12:47 +0900

    add：収支グラフページでヘッダーとフッターの有効を確認

---
+++ app/pages/balance/home.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Header from "../../components/Header/Header";
+import Hooter from "../../components/Footer/Footer";
+export default function Home() {
+    return (
+        <div>
+            <Header />
+            <Hooter />
+        </div>
+    );
+}
