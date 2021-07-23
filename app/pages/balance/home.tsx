52e3ddf7d1e256ebd5f30ce6d5e8245e1ddfbba9
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-23 21:44:27 +0900

    add：footer表示

--- app/pages/balance/home.tsx
+++ app/pages/balance/home.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Header from "../../components/Header/Header";
-import Hooter from "../../components/Footer/Footer";
+import Footer from "../../components/Footer/Footer";
 export default function Home() {
     return (
         <div>
             <Header />
-            <Hooter />
+            <Footer />
+
+            {/* <UserInputForm /> */}
         </div>
     );
 }
