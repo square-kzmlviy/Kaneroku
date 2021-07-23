430a85bc49da1602134507680c2749f2513123c1
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-23 21:42:37 +0900

    add：themeを利用できる機能

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,8 +1,9 @@
-import "../styles/globals.css";
 import React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
 
 export default function MyApp(props) {
     const { Component, pageProps } = props;
@@ -24,9 +25,11 @@ export default function MyApp(props) {
                     content="minimum-scale=1, initial-scale=1, width=device-width"
                 />
             </Head>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline />
-            <Component {...pageProps} />
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
         </React.Fragment>
     );
 }
