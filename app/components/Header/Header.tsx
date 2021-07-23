4ba3404b6219518453e90de1ddd65298297d0b92
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-23 21:43:23 +0900

    add：header

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import { useGetLogInUser } from "../../components/hooks/useGetLogInUser";
-import style from "../../styles/sign_up.module.css";
+import style from "../../styles/header.module.css";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 export default function Header() {
     const { getLogInUser, loginUserName, loginStatus, loginUserId } = useGetLogInUser();
@@ -13,7 +13,7 @@ export default function Header() {
         <AppBar position="static">
             <Toolbar className={style.header_toolber}>
                 <img src="/static/logo.svg" width="auto" height="40px" />
-                <div className={style.header_toolber}>
+                <div className={style.header_user}>
                     <AccountCircleIcon />
                     {loginUserName}
                 </div>
