2b70d55531d4710474883baab127662270725953
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-24 02:23:33 +0900

    add：ユーザーが登録しているカテゴリ一覧表示機能

--- api/app/controllers/categories_controller.rb
+++ api/app/controllers/categories_controller.rb
@@ -1,3 +1,9 @@
 class CategoriesController < ApplicationController
-
+  def index
+    if current_user
+      render json: current_user.categories, status: :ok
+    else
+      render json: { data: [], message: 'ユーザーが存在しません' }, status: :no_content
+    end
+  end
 end
