9b345305922d1af56d939daa2638a89a7575bbe8
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-23 21:43:43 +0900

    add：footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,28 +1,27 @@
 import React, { useState, useEffect } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import { useGetLogInUser } from "../hooks/useGetLogInUser";
 import style from "../../styles/footer.module.css";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
+
+const useStyles = makeStyles({
+    appBar: {
+        top: "auto",
+        bottom: 0,
+    },
+    fabButton: {
+        position: "absolute",
+        zIndex: 1,
+        top: -30,
+        left: 0,
+        right: 0,
+        margin: "0 auto",
+    },
+});
 export default function Footer() {
-    const useStyles = makeStyles((theme: Theme) =>
-        createStyles({
-            appBar: {
-                top: "auto",
-                bottom: 0,
-            },
-            fabButton: {
-                position: "absolute",
-                zIndex: 1,
-                top: -30,
-                left: 0,
-                right: 0,
-                margin: "0 auto",
-            },
-        })
-    );
     const classes = useStyles();
     return (
         <AppBar position="fixed" color="primary" className={classes.appBar}>
