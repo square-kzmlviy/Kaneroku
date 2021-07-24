95a1c1f23c493671319a2badd520edb8a574f81e
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-25 02:36:17 +0900

    add：モーダル表示部

--- app/pages/balance/home.tsx
+++ app/pages/balance/home.tsx
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import BalanceInput from "../../components/modal/BalanceInput";
 export default function Home() {
+    const [open, setOpen] = React.useState(false);
+
+    function handleClickOpen() {
+        setOpen(true);
+    }
+
+    function handleClose() {
+        setOpen(false);
+    }
     return (
         <div>
             <Header />
-            <Footer />
+            <BalanceInput open={open} onClose={handleClose} />
+            <Footer handleClickOpen={handleClickOpen} />
 
             {/* <UserInputForm /> */}
         </div>
