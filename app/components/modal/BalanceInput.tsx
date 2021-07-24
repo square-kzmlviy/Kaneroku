39764bf418342db5defee5effec0f853b4d1fb3f
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-25 02:28:43 +0900

    add：インポート文

--- app/components/modal/BalanceInput.tsx
+++ app/components/modal/BalanceInput.tsx
@@ -1,3 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import { RepositoryFactory } from "../../repositories/RepositoryFactory";
+const categoryRepository = RepositoryFactory.get("category");
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
 
 
 export default function BalanceInput(props: BalanceInput) {}
