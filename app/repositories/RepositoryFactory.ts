67102f473fac166992e566db9bf6ae79202989db
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2021-07-26 20:07:52 +0900

    add：balanceRepository

--- app/repositories/RepositoryFactory.ts
+++ app/repositories/RepositoryFactory.ts
@@ -1,10 +1,12 @@
 import UserRepository from "./userRepository";
 import SessionRepository from "./sessionRepository";
 import CategoryRepository from "./categoryRepository";
+import BalanceRepository from "./balanceRepository";
 const repositories = {
     users: UserRepository,
     session: SessionRepository,
     category: CategoryRepository,
+    balance: BalanceRepository,
 };
 
 export const RepositoryFactory = {
