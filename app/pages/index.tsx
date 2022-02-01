553c8d9c30f44b2c432ec1d2d5a5f61c423862ea
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2022-02-01 19:06:59 +0900

    add env

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className={style.main_container}>
             <img src="/static/index_logo.svg" width="85%" height="auto" />
-            <p>自動は万能ではない{process.env.HOST}</p>
+            <p>自動は万能ではない{process.env.NEXT_PUBLIC_HOST_NAME}</p>
             <p>節制とは、小さな努力の積み重ね</p>
             <Link href="/user/log_in">ログイン</Link>
             <Link href="/user/sign_up">新規登録</Link>
--- app/repositories/Repository.ts
+++ app/repositories/Repository.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseDomein: string = process.env.HOST;
+const baseDomein: string = process.env.NEXT_PUBLIC_HOST_NAME;
 export default axios.create({
     baseURL: baseDomein,
     withCredentials: true,
