14561107785999791b3a9eaadc3391e48a740e27
Kazuma Inoue <kazuma.inoue.hal.nagoya@gmail.com>
2022-02-02 23:02:04 +0900

    fix cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8000'
+    origins 'http://localhost:8000', 'https://kaneroku-app.com/'
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
